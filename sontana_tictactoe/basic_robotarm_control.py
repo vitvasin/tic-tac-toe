871587d3a77b31e5049b3c1bd2490b04c892430c
vitvasin <vasin22315@hotmail.com>
2025-02-07 14:41:13 +0700

    Add basic Realsense functionality and clear fault handling scripts

---
+++ sontana_tictactoe/basic_robotarm_control.py
@@ -0,0 +1,428 @@
+#! /usr/bin/env python3
+
+###
+# KINOVA (R) KORTEX (TM)
+#
+# Copyright (c) 2018 Kinova inc. All rights reserved.
+#
+# This software may be modified and distributed
+# under the terms of the BSD 3-Clause license.
+#
+# Refer to the LICENSE file for details.
+#
+###
+import math
+import sys
+import os
+import time
+import threading
+
+from kortex_api.autogen.client_stubs.BaseClientRpc import BaseClient
+from kortex_api.autogen.client_stubs.BaseCyclicClientRpc import BaseCyclicClient
+
+from kortex_api.autogen.messages import Base_pb2, BaseCyclic_pb2, Common_pb2
+
+# Maximum allowed waiting time during actions (in seconds)
+TIMEOUT_DURATION = 20
+
+# Create closure to set an event after an END or an ABORT
+def check_for_end_or_abort(e):
+    """Return a closure checking for END or ABORT notifications
+
+    Arguments:
+    e -- event to signal when the action is completed
+        (will be set when an END or ABORT occurs)
+    """
+    def check(notification, e = e):
+        print("EVENT : " + \
+              Base_pb2.ActionEvent.Name(notification.action_event))
+        if notification.action_event == Base_pb2.ACTION_END \
+        or notification.action_event == Base_pb2.ACTION_ABORT:
+            e.set()
+    return check
+ 
+def example_move_to_home_position(base):
+    # Make sure the arm is in Single Level Servoing mode
+    base_servo_mode = Base_pb2.ServoingModeInformation()
+    base_servo_mode.servoing_mode = Base_pb2.SINGLE_LEVEL_SERVOING
+    base.SetServoingMode(base_servo_mode)
+    
+    # Move arm to ready position
+    print("Moving the arm to a safe position")
+    action_type = Base_pb2.RequestedActionType()
+    action_type.action_type = Base_pb2.REACH_JOINT_ANGLES
+    action_list = base.ReadAllActions(action_type)
+    action_handle = None
+    for action in action_list.action_list:
+        if action.name == "Home":
+            action_handle = action.handle
+
+    if action_handle == None:
+        print("Can't reach safe position. Exiting")
+        return False
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(
+        check_for_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+
+    base.ExecuteActionFromReference(action_handle)
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+
+    if finished:
+        print("Safe position reached")
+    else:
+        print("Timeout on action notification wait")
+    return finished
+
+def example_angular_action_movement(base):
+    
+    print("Starting angular action movement ...")
+    action = Base_pb2.Action()
+    action.name = "Example angular action movement"
+    action.application_data = ""
+
+    actuator_count = base.GetActuatorCount()
+
+    # Place arm straight up
+    for joint_id in range(actuator_count.count):
+        joint_angle = action.reach_joint_angles.joint_angles.joint_angles.add()
+        joint_angle.joint_identifier = joint_id
+        joint_angle.value = 0
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(
+        check_for_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+    
+    print("Executing action")
+    base.ExecuteAction(action)
+
+    print("Waiting for movement to finish ...")
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+
+    if finished:
+        print("Angular movement completed")
+    else:
+        print("Timeout on action notification wait")
+    return finished
+
+def example_angular_action_test(base):
+    
+    print("Starting angular action movement ...")
+    action = Base_pb2.Action()
+    action.name = "Example angular action movement"
+    action.application_data = ""
+
+    actuator_count = base.GetActuatorCount()
+
+    joint_values = [322,291,235,137,305,50] # Example values for each joint
+
+    for joint_id in range(actuator_count.count):
+        joint_angle = action.reach_joint_angles.joint_angles.joint_angles.add()
+        joint_angle.joint_identifier = joint_id
+        joint_angle.value = joint_values[joint_id]
+        
+        
+    # Place arm on home
+    
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(
+        check_for_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+    
+    print("Executing action")
+    base.ExecuteAction(action)
+
+    print("Waiting for movement to finish ...")
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+
+    if finished:
+        print("Angular movement completed")
+    else:
+        print("Timeout on action notification wait")
+    return finished
+    
+
+def example_cartesian_action_movement(base, base_cyclic):
+    
+    print("Starting Cartesian action movement ...")
+    action = Base_pb2.Action()
+    action.name = "Example Cartesian action movement"
+    action.application_data = ""
+
+    feedback = base_cyclic.RefreshFeedback()
+
+    cartesian_pose = action.reach_pose.target_pose
+    cartesian_pose.x = feedback.base.tool_pose_x          # (meters)
+    cartesian_pose.y = feedback.base.tool_pose_y - 0.1    # (meters)
+    cartesian_pose.z = feedback.base.tool_pose_z - 0.2    # (meters)
+    cartesian_pose.theta_x = feedback.base.tool_pose_theta_x # (degrees)
+    cartesian_pose.theta_y = feedback.base.tool_pose_theta_y # (degrees)
+    cartesian_pose.theta_z = feedback.base.tool_pose_theta_z # (degrees)
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(
+        check_for_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+
+    print("Executing action")
+    base.ExecuteAction(action)
+
+    print("Waiting for movement to finish ...")
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+
+    if finished:
+        print("Cartesian movement completed")
+    else:
+        print("Timeout on action notification wait")
+    return finished
+
+
+    
+    
+    
+    
+    
+def draw_circle_on_xz_plane(radius, center_y, center_z, num_points):
+    points = []
+    for i in range(num_points):
+        angle = 2 * math.pi * i / num_points
+        y = center_y + radius * math.cos(angle)
+        z = center_z + radius * math.sin(angle)
+        points.append((y, z))
+    return points
+
+def move_robot_arm_to(base, base_cyclic, x, y, z, theta_x, theta_y, theta_z):
+    action = Base_pb2.Action()
+    action.name = "Move to point"
+    action.application_data = ""
+
+    feedback = base_cyclic.RefreshFeedback()
+
+    cartesian_pose = action.reach_pose.target_pose
+    cartesian_pose.x = x
+    cartesian_pose.y = y
+    cartesian_pose.z = z
+    cartesian_pose.theta_x = theta_x
+    cartesian_pose.theta_y = theta_y
+    cartesian_pose.theta_z = theta_z
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(
+        check_for_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+
+    base.ExecuteAction(action)
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+
+    return finished
+
+
+def draw_circle_XZ(base, base_cyclic, radius, num_points):
+    print("Starting Cartesian action movement ...")
+    
+    feedback = base_cyclic.RefreshFeedback()
+    center_x = feedback.base.tool_pose_x
+    center_y = feedback.base.tool_pose_y 
+    center_z = feedback.base.tool_pose_z 
+    theta_x = feedback.base.tool_pose_theta_x
+    theta_y = feedback.base.tool_pose_theta_y
+    theta_z = feedback.base.tool_pose_theta_z
+
+    move_robot_arm_to(base, base_cyclic, center_x+0.05, center_y, center_z, theta_x, theta_y, theta_z)
+    time.sleep(0.5)
+    
+    # radius = 0.1  # Example radius
+    # num_points = 100  # Number of points to draw the circle
+    circle_points = draw_circle_on_xz_plane(radius, center_y, center_z, num_points)
+
+    for point in circle_points:
+        y, z = point
+        success = move_robot_arm_to(base, base_cyclic, center_x, y, z, theta_x, theta_y, theta_z)
+        if not success:
+            print("Failed to move to point:", point)
+            break
+        #time.sleep(0.01)
+
+    print("Finished drawing circle on XZ plane")
+    move_robot_arm_to(base, base_cyclic, center_x+0.02, center_y, center_z, theta_x, theta_y, theta_z)
+    time.sleep(0.5)
+    move_robot_arm_to(base, base_cyclic, center_x, center_y, center_z, theta_x, theta_y, theta_z)
+
+def draw_cross_XZ(base, base_cyclic, radius, num_points):
+    print("Starting Cartesian action movement to draw cross ...")
+    
+    feedback = base_cyclic.RefreshFeedback()
+    center_x = feedback.base.tool_pose_x
+    center_y = feedback.base.tool_pose_y 
+    center_z = feedback.base.tool_pose_z 
+    theta_x = feedback.base.tool_pose_theta_x
+    theta_y = feedback.base.tool_pose_theta_y
+    theta_z = feedback.base.tool_pose_theta_z
+    
+    move_robot_arm_to(base, base_cyclic, center_x+0.05, center_y, center_z, theta_x, theta_y, theta_z)
+    time.sleep(1)
+
+    # Calculate points for the cross
+    cross_points = []
+    step = radius / (num_points // 2)
+    
+    # Diagonal line 1
+    for i in range(-num_points // 2, num_points // 2 + 1):
+        if i == 0:
+            continue
+        cross_points.append((center_y + i * step, center_z + i * step))
+    
+    for point in cross_points:
+        y, z = point
+        success = move_robot_arm_to(base, base_cyclic, center_x, y, z, theta_x, theta_y, theta_z)
+        if not success:
+            print("Failed to move to point:", point)
+            break
+        #time.sleep(0.01)
+
+    # Move back to center before drawing the second diagonal line
+    success = move_robot_arm_to(base, base_cyclic, center_x+0.05, center_y, center_z, theta_x, theta_y, theta_z)
+    if not success:
+        print("Failed to move back to center")
+        return
+    #time.sleep(1)
+
+    # Diagonal line 2
+    cross_points = []
+    for i in range(-num_points // 2, num_points // 2 + 1):
+        if i == 0:
+            continue
+        cross_points.append((center_y + i * step, center_z - i * step))
+
+    for point in cross_points:
+        y, z = point
+        success = move_robot_arm_to(base, base_cyclic, center_x, y, z, theta_x, theta_y, theta_z)
+        if not success:
+            print("Failed to move to point:", point)
+            break
+        #time.sleep(0.01)
+
+    print("Finished drawing cross on XZ plane")
+    move_robot_arm_to(base, base_cyclic, center_x+0.05, center_y, center_z, theta_x, theta_y, theta_z)
+
+def draw_circle_with_twist_command(base,base_cyclic, radius, duration):
+    
+    print("Starting Cartesian action movement to draw cross ...")
+    
+    feedback = base_cyclic.RefreshFeedback()
+    center_x = feedback.base.tool_pose_x
+    center_y = feedback.base.tool_pose_y 
+    center_z = feedback.base.tool_pose_z 
+    theta_x = feedback.base.tool_pose_theta_x
+    theta_y = feedback.base.tool_pose_theta_y
+    theta_z = feedback.base.tool_pose_theta_z
+    
+    move_robot_arm_to(base, base_cyclic, center_x+0.05, center_y, center_z, theta_x, theta_y, theta_z)
+    time.sleep(0.5)
+    move_robot_arm_to(base, base_cyclic, center_x, center_y+0.02, center_z, theta_x, theta_y, theta_z)
+    time.sleep(0.5)
+    
+    command = Base_pb2.TwistCommand()
+    command.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_MIXED
+    command.duration = 0
+
+    twist = command.twist
+    twist.linear_x = 0
+    twist.angular_x = 0
+    twist.angular_y = 0
+    twist.angular_z = 0
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        elapsed_time = time.time() - start_time
+        twist.linear_y = radius * math.cos(2 * math.pi * elapsed_time / duration)
+        twist.linear_z = radius * math.sin(2 * math.pi * elapsed_time / duration)
+        twist.angular_x = 2 * math.pi / duration
+
+        base.SendTwistCommand(command)
+        #time.sleep(0.1)  # Adjust the sleep time as needed
+
+    print("Stopping the robot...")
+    base.Stop()
+    time.sleep(1)
+    
+    move_robot_arm_to(base, base_cyclic, center_x+0.05, center_y, center_z, theta_x, theta_y, theta_z)
+    time.sleep(0.5)
+    move_robot_arm_to(base, base_cyclic, center_x, center_y, center_z, theta_x, theta_y, theta_z)
+
+    return True
+
+def main():
+    radius = 0.1  # Example radius
+    num_points = 5  # Number of points to draw the circle
+    
+    # Import the utilities helper module
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+    import utilities
+
+    # Parse arguments
+    args = utilities.parseConnectionArguments()
+    
+    # Create connection to the device and get the router
+    with utilities.DeviceConnection.createTcpConnection(args) as router:
+
+        # Create required services
+        base = BaseClient(router)
+        base_cyclic = BaseCyclicClient(router)
+
+        # Example core
+        success = True
+
+        #success &= example_move_to_home_position(base)
+        #success &= example_cartesian_action_movement(base, base_cyclic)
+        #success &= example_angular_action_movement(base)
+        #delay 5 sec
+        #time.sleep(5)
+        success &= example_angular_action_test(base)
+        time.sleep(1)
+        #move_robot_arm_to(base, base_cyclic, -0.4, 0.015, 0.38, -120, 80, 60)
+        #success &= circle_trajectory(base, base_cyclic,0.1,10)
+        
+        
+        
+        # feedback = base_cyclic.RefreshFeedback()
+
+        # X= feedback.base.tool_pose_x          # (meters)
+        # Y = feedback.base.tool_pose_y    # (meters)
+        # Z = feedback.base.tool_pose_z     # (meters)
+        # RX = feedback.base.tool_pose_theta_x # (degrees)
+        # RY = feedback.base.tool_pose_theta_y # (degrees)
+        # RZ = feedback.base.tool_pose_theta_z # (degrees)
+        
+        # print(X,Y,Z,RX,RY,RZ)
+        # time.sleep(2)
+        # draw_circle_XZ(base, base_cyclic, 0.02, 16)
+        # # #time.sleep(2)
+        # draw_cross_XZ(base, base_cyclic, 0.01, 2)
+        time.sleep(2)
+        draw_circle_with_twist_command(base,base_cyclic, 0.15, 1)
+        #time.sleep(2)
+        #draw_cross_XZ(base, base_cyclic, 0.01, 2)
+        #note 
+        # joint_values = [334,284,225,128,316,318] # Example values for each joint
+        #pose = [-179,15,384,120,80,60]
+
+        # You can also refer to the 110-Waypoints examples if you want to execute
+        # a trajectory defined by a series of waypoints in joint space or in Cartesian space
+
+        return 0 if success else 1
+
+if __name__ == "__main__":
+    exit(main())
---
+++ sontana_tictactoe/clear_fault.py
@@ -0,0 +1,71 @@
+# import kortex_api.autogen as k_api
+# # Create API objects
+# transport = k_api.TransportClientTcp()
+# router = k_api.RouterClient(transport)
+# transport.connect("robot_ip", k_api.DEFAULT_PORT)
+
+# # Create session
+# session_info = k_api.Session.CreateSessionInfo()
+# session_info.username = "admin"
+# session_info.password = "admin"
+# session_info.session_inactivity_timeout = 60000
+# session_manager = k_api.SessionManager(router)
+# session_manager.CreateSession(session_info)
+
+# # Get safety information
+# device_config = k_api.DeviceConfig(router)
+# safety_handle = k_api.Common.SafetyHandle()
+# safety_info = device_config.GetSafetyInformation(safety_handle)
+
+# # Check fault flags
+# fault_flags = safety_info.fault_flags
+
+# # Clear specific safety status
+# device_config.ClearSafetyStatus(safety_handle)
+
+# # Clear all safety statuses
+# device_config.ClearAllSafetyStatus()
+
+#! /usr/bin/env python3
+
+###
+# KINOVA (R) KORTEX (TM)
+#
+# Copyright (c) 2018 Kinova inc. All rights reserved.
+#
+# This software may be modified and distributed
+# under the terms of the BSD 3-Clause license.
+#
+# Refer to the LICENSE file for details.
+#
+###
+
+import sys
+import os
+
+from kortex_api.autogen.client_stubs.DeviceManagerClientRpc import DeviceManagerClient
+from kortex_api.autogen.client_stubs.DeviceConfigClientRpc import DeviceConfigClient
+
+
+def main():
+    # Import the utilities helper module
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+    import utilities
+
+    # Parse arguments
+    args = utilities.parseConnectionArguments()
+    
+    # Create connection to the device and get the router
+    with utilities.DeviceConnection.createTcpConnection(args) as router:
+
+        # Create required services
+        device_manager = DeviceManagerClient(router)
+        device_config = DeviceConfigClient(router)
+
+        # Example core
+        #example_routed_device_config(device_manager, device_config)
+        device_config.ClearAllSafetyStatus()
+
+if __name__ == "__main__":
+    main()
+    
